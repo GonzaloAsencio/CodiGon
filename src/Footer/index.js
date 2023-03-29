ddae72ab3e5eb407d7de2e751a9794a98a94d60c
Luciano Franchino <luciano_franchino@hotmail.com>
2023-03-29 20:31:02 -0300

    Create footer folder and html structure

---
+++ src/Footer/index.js
@@ -0,0 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import './footer.model.css';
+
+function Footer() {
+    return(
+        <><div>
+            <p>Linea de imagenes</p>
+        </div>
+        <footer className='footer'>
+            <div className='linksContainer'>
+                <a>Quienes Somos</a>
+                <a>Contacto</a>
+                <a>Nuestras Redes</a>
+            </div>
+            <div className='logoContainer'>
+                <p>LOGO</p>
+            </div>
+        </footer></>
+    );
+}
+
+export default Footer;
