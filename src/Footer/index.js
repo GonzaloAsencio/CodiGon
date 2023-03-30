95a6f60429bc191996ac923da9f6744e609511bb
Luciano Franchino <luciano_franchino@hotmail.com>
2023-03-30 15:55:05 -0300

    final drop down

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,41 +1,20 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './footer.model.css';
-import React, {useState} from 'react';
 
 function Footer() {
-
-        const [isOpen, setIsOpen] = useState(false);
-        const handleSelectChange = (event) => {
-            console.log("Selected value:", event.target.value);
-            setIsOpen(false);
-        };
-        const handleMenuClick = () => {
-            setIsOpen(!isOpen);
-        };
-        const handleMenuBlur = () => {
-            setIsOpen(false);
-        };
 
     return(
         <footer className='footer'>
             <div className='linksContainer'>
                 <a>Quienes Somos</a>
                 <a>Contacto</a>
-                <div
-                    className="menu"
-                    tabIndex="0"
-                    onClick={handleMenuClick}
-                    onBlur={handleMenuBlur}
-                >
-                    <select id="menu" value="" onChange={handleSelectChange}>
-                        <option value="" disabled hidden>
-                        Nuestras Redes
-                        </option>
-                        <option value="YouTube">YouTube</option>
-                        <option value="GitHub">GitHub</option>
-                        <option value="Twitter">Twitter</option>
-                        <option value="Linkedin">Linkedin</option>
-                    </select>
+                <div class="dropdown">
+                    <span>Nuestras Redes</span>
+                    <div class="dropdown-content">
+                    <a href="#">GitHub</a>
+                    <a href="https://www.youtube.com/">YouTube</a>
+                    <a href="#">Twitter</a>
+                    </div>
                 </div>
             </div>
             <div className='logoContainer'>
