a2d1a23f34c75ab1bfbd649595517cc85a1c6b14
Luciano Franchino <luciano_franchino@hotmail.com>
2023-03-30 14:48:49 -0300

    Drop down menu in social media

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,21 +1,47 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import './footer.model.css';
+import React, {useState} from 'react';
 
 function Footer() {
+
+        const [isOpen, setIsOpen] = useState(false);
+        const handleSelectChange = (event) => {
+            console.log("Selected value:", event.target.value);
+            setIsOpen(false);
+        };
+        const handleMenuClick = () => {
+            setIsOpen(!isOpen);
+        };
+        const handleMenuBlur = () => {
+            setIsOpen(false);
+        };
+
     return(
-        <><div>
-            <p>Linea de imagenes</p>
-        </div>
         <footer className='footer'>
             <div className='linksContainer'>
                 <a>Quienes Somos</a>
                 <a>Contacto</a>
-                <a>Nuestras Redes</a>
+                <div
+                    className="menu"
+                    tabIndex="0"
+                    onClick={handleMenuClick}
+                    onBlur={handleMenuBlur}
+                >
+                    <select id="menu" value="" onChange={handleSelectChange}>
+                        <option value="" disabled hidden>
+                        Nuestras Redes
+                        </option>
+                        <option value="YouTube">YouTube</option>
+                        <option value="GitHub">GitHub</option>
+                        <option value="Twitter">Twitter</option>
+                        <option value="Linkedin">Linkedin</option>
+                    </select>
+                </div>
             </div>
             <div className='logoContainer'>
                 <p>LOGO</p>
             </div>
-        </footer></>
+        </footer>
     );
 }
 
