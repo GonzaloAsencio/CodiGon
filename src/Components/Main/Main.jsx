1956b1557b8eb65c2c6087bc5ca93e842f006bc4
Gonzalo Asencio <gestorgonzalo@gmail.com>
2023-04-25 12:21:32 -0300

    add-slider

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -4,6 +4,7 @@ import IconSeparetor from '../SharedComponents/IconSeparetor/IconSeparetor';
 import Carousel from '../SharedComponents/Carousel/Carousel';
 import Card from '../SharedComponents/Card/Cards';
 import iconAmount from '../SharedComponents/IconSeparetor/IconCounter';
+import Slider from '../SharedComponents/Slider/Slider'
 
 function Main() {
   return (
@@ -11,12 +12,17 @@ function Main() {
         <div className='images-carousell'>
           <Carousel/>
         </div>
+        <div className='slider-conteiner'>
+            <Slider/>
+          </div>
         <div className='images-select'>
           <div className='tutorials'>
             <h2>TUTORIALES</h2>
+          </div>
         </div>
-        </div>
-          <Card/>
+          <div className='card-conteiner'>
+            <Card/>
+          </div>
         <div className='see-more-tutorials'>
             <ul>
               <li>
