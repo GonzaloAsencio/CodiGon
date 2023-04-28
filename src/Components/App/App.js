b6a93c91374b0084d665f20f72207f677f7a68a5
Gonzalo Asencio <gestorgonzalo@gmail.com>
2023-04-28 15:36:41 -0300

    add-react-router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,16 +1,26 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Header from '../SharedComponents/Header/Header';
 import Tutorial from '../TutorialPage/Tutorial'
 import Main from '../Main/Main';
 import Footer from '../SharedComponents/Footer/index';
 
+
+
+
 function App() {
   return (
-  <>
+  <Router>
     <Header/>
-    <Tutorial/>
+    <Routes>
+       <Route path='/' exact Component={Main}/>
+       <Route path='/home' exact Component={Main}/>
+       <Route path='/tutorial' exact Component={Tutorial}/>
+       <Route path="/*" element={<Navigate to="/" />} />
+    </Routes>
     <Footer/>
-  </>
-
+  </Router>
   );
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Main.css';
+import { Link } from 'react-router-dom';
 import IconSeparetor from '../SharedComponents/IconSeparetor/IconSeparetor';
 import Carousel from '../SharedComponents/Carousel/Carousel';
 import Card from '../SharedComponents/Card/Cards';
@@ -21,12 +22,14 @@ function Main() {
           </div>
         </div>
           <div className='card-conteiner'>
-            <Card/>
+            <Card search={""}/>
           </div>
         <div className='see-more-tutorials'>
             <ul>
               <li>
-                <a href='' data-text="&nbsp;Ver&nbsp;mas">&nbsp;ver mas&nbsp;</a>
+                <Link to="/tutorial">
+                <a data-text="&nbsp;Ver&nbsp;mas">&nbsp;ver mas&nbsp;</a>
+                </Link>
               </li>
             </ul>
         </div>
