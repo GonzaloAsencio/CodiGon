c4136f6426c897846f8146520f22bb901a8959ba
Gonzalo Asencio <51496564+GonzaloAsencio@users.noreply.github.com>
2023-06-02 12:27:31 -0300

    add-inputs-validations

---
+++ src/Components/SharedComponents/Contact/inputsValidatios.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default  function (values) {
+    const errors = {};
+
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (values.fullname === '') {
+      errors.fullname = 'El nombre es requerido';
+    }
+    if (!values.email) {
+      errors.email = 'El email es requerido';
+    } else if (!regex.test(values.email)) {
+      errors.email = 'El email no es un formato valido';
+    }
+    if (!values.message) {
+      errors.message = "El mensaje es requerido";
+    } else if (values.message.length < 4) {
+      errors.message = 'El mensaje debe tener mas de 4 caracteres';
+    }
+    return errors;
+  };
