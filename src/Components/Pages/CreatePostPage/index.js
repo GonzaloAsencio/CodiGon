becc8a99dfd17bc2677501fd38c40a9f77a90d8b
Gonzalo Asencio <51496564+GonzaloAsencio@users.noreply.github.com>
2023-07-17 16:25:20 -0300

    change-local-host

--- src/Components/Pages/CreatePostPage/index.js
+++ src/Components/Pages/CreatePostPage/index.js
@@ -28,7 +28,7 @@ export default function CreatePost() {
     data.set('icon',icon);
     ev.preventDefault();
 
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://codigon.onrender.com/post', {
       method: 'POST',
       body: data,
       credentials: 'include',
